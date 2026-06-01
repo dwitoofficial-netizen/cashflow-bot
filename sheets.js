06d9ed7439bf645bd5427a4e4b0c03db4f51d1e0
dwitoofficial-netizen <dwito.official@gmail.com>
2026-06-01 19:17:22 +0700

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -4,12 +4,7 @@ const { google } = require("googleapis");
 // AUTH SETUP
 // =====================
 const auth = new google.auth.GoogleAuth({
-  credentials: {
-    type: "service_account",
-    project_id: process.env.GOOGLE_PROJECT_ID,
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  },
+  credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS),
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 
